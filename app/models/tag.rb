02ef7e00dec0ed5b62db9b8092550ba1684e68a2
Amit Kumar <amitkumar4436@gmail.com>
2015-06-27 00:34:03 +0530

    Fixed tagging issue

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,11 +1,4 @@
 class Tag < ActiveRecord::Base
 	has_many :taggings
 	has_many :posts, through: :taggings
-
-	def tag_list
-	  self.tags.collect do |tag|
-	    tag.name
-	  end.join(", ")
-	end
-
 end
